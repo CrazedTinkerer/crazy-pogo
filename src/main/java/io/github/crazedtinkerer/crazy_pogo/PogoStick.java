4118f264fba0b08b2915dcc885ad5eb8a360492d
Talinkri <110867557+CrazedTinkerer@users.noreply.github.com>
2025-08-24 22:50:13 -0500

    Make pogo stick work in adventure mode

--- src/main/java/io/github/crazedtinkerer/crazy_pogo/PogoStick.java
+++ src/main/java/io/github/crazedtinkerer/crazy_pogo/PogoStick.java
@@ -3,20 +3,37 @@ package io.github.crazedtinkerer.crazy_pogo;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.UseCooldownComponent;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class PogoStick extends Item {
 	public PogoStick(Settings settings) {
 		super(settings);
+	}
+
+	@Override
+	public ActionResult use(World world, PlayerEntity user, Hand hand) {
+		// This would use useOnBlock instead, but that doesn't work in adventure mode
+		// without canPlaceOn, and the pogo stick should still be usable there
+		ItemStack usedItem = user.getStackInHand(hand);
+		HitResult hitResult = user.raycast(user.getAttributeValue(EntityAttributes.BLOCK_INTERACTION_RANGE), 0, false);
+
+		if (hitResult.getType() == HitResult.Type.BLOCK){
+            bounceUser(user);
+			applyCooldown(usedItem, user);
+			return ActionResult.SUCCESS;
+		} else {
+			return super.use(world, user, hand);
+		}
 	}
 
 	@Override
@@ -33,19 +50,6 @@ public class PogoStick extends Item {
 		entity.addVelocity(bounceVelocity);
 
 		applyCooldown(stack, user);
-		return ActionResult.SUCCESS;
-	}
-
-	@Override
-	public ActionResult useOnBlock(ItemUsageContext context){
-		LivingEntity user = context.getPlayer();
-		if (user == null){
-			return ActionResult.PASS;
-		}
-
-		bounceUser(user);
-
-		applyCooldown(context.getStack(), user);
 		return ActionResult.SUCCESS;
 	}
 
